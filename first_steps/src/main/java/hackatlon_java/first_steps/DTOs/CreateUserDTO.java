1e1d7b01b3867ea7afbad977ff9a19e8407f2693
petra <halouzkovap@seznam.cz>
2020-07-16 15:13:30 +0200

    dto validation

--- first_steps/src/main/java/hackatlon_java/first_steps/DTOs/CreateUserDTO.java
+++ first_steps/src/main/java/hackatlon_java/first_steps/DTOs/CreateUserDTO.java
@@ -1,9 +1,28 @@
 package hackatlon_java.first_steps.DTOs;
 
+
+import hackatlon_java.first_steps.Validation.ValidEmail;
+import hackatlon_java.first_steps.Validation.ValidPassword;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
 public class CreateUserDTO {
+
+    @NotBlank(message = "Username is required")
     private String name;
+
+    @ValidEmail(message = "Valid Email is required")
     private String email;
+
+    @NotBlank(message = "Password is required.")
+    @ValidPassword
     private String password;
+
 
     public CreateUserDTO(String name, String email, String password) {
         this.name = name;
--- first_steps/src/main/java/hackatlon_java/first_steps/DTOs/QuestionDTO.java
+++ first_steps/src/main/java/hackatlon_java/first_steps/DTOs/QuestionDTO.java
@@ -3,9 +3,10 @@ package hackatlon_java.first_steps.DTOs;
 import lombok.Getter;
 import lombok.Setter;
 
+@Getter
+@Setter
 public class QuestionDTO {
-    @Getter
-    @Setter
+
     private String question;
     private String description;
     private String title;
