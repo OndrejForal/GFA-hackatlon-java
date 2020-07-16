cebd6d0c4df4fb361a5b9861aae12d7b55cc019d
prandyCZ <hlusicka@gmail.com>
2020-07-16 15:04:22 +0200

    add setters to userDTO

--- first_steps/src/main/java/hackatlon_java/first_steps/DTOs/CreateUserDTO.java
+++ first_steps/src/main/java/hackatlon_java/first_steps/DTOs/CreateUserDTO.java
@@ -1,8 +1,10 @@
 package hackatlon_java.first_steps.DTOs;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class CreateUserDTO {
     private String name;
     private String email;
