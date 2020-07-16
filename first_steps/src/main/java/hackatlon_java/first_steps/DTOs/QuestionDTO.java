19a6ba8b32a7d9d50cbefefef85004272d2cf429
petra <halouzkovap@seznam.cz>
2020-07-16 17:16:13 +0200

    add questin controller

--- first_steps/src/main/java/hackatlon_java/first_steps/DTOs/QuestionDTO.java
+++ first_steps/src/main/java/hackatlon_java/first_steps/DTOs/QuestionDTO.java
@@ -10,5 +10,12 @@ public class QuestionDTO {
     private String question;
     private String description;
     private String title;
+    private String url;
 
+    public QuestionDTO(String question, String description, String title, String url) {
+        this.question = question;
+        this.description = description;
+        this.title = title;
+        this.url = url;
+    }
 }
